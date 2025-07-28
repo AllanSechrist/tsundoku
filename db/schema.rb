efb6b90367dfed93ccbb5bc6194384b763c9857a
Allan Sechrist <sechristallan@gmail.com>
2025-07-28 11:55:52 +0900

    Add profile model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_28_023203) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_28_025438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_28_023203) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -61,4 +69,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_28_023203) do
   add_foreign_key "books", "authors"
   add_foreign_key "books", "genres"
   add_foreign_key "books", "publishers"
+  add_foreign_key "profiles", "users"
 end
