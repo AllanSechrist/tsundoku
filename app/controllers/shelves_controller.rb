4c789d503306da38c5484f604bb644e3e6bb69fa
Allan Sechrist <sechristallan@gmail.com>
2025-08-06 21:20:02 +0900

    Refactor shelves controller.

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,5 +1,5 @@
 class ShelvesController < ApplicationController
-  before_action :get_profile
+  before_action :set_profile
   def index
     if @profile
       render json: ShelfSerializer.new(@profile.shelves).serializable_hash
@@ -9,62 +9,55 @@ class ShelvesController < ApplicationController
   end
 
   def show
-    if @profile
-      shelf = @profile.shelves.find_by(id: params[:id])
+    return render json: { message: "Profile not found" }, status: :not_found unless @profile
+    shelf = @profile.shelves.find_by(id: params[:id])
 
-      if shelf
-        render json: ShelfSerializer.new(shelf, include: [:shelf_books, :owned_books]).serializable_hash
-      else
-        render json: { message: "Shelf not found" }, status: :not_found
-      end
-
+    if shelf
+      render json: ShelfSerializer.new(shelf, include: [:shelf_books, :owned_books]).serializable_hash status: :ok
     else
-      render json: { message: "Profile not found" }, status: :not_found
+      render json: { message: "Shelf not found" }, status: :not_found
     end
   end
 
   def create
-    if @profile
-      shelf = @profile.shelves.new(shelf_params)
-      if shelf.save
-        render json: ShelfSerializer.new(shelf).serializable_hash, status: :created
-      else
-        render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
-      end
+    return render json: { message: "Profile not found" }, status: :not_found unless @profile
+    shelf = @profile.shelves.new(shelf_params)
+    if shelf.save
+      render json: ShelfSerializer.new(shelf).serializable_hash, status: :created
     else
-      render json: { message: "Profile not found" }, status: :not_found
+      render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def update
-    if @profile
-      shelf = @profile.shelves.find_by(id: params[:id])
-      if shelf.update(shelf_params)
-        render json: ShelfSerializer.new(shelf, include: [:shelf_books, :owned_books]).serializable_hash, status: :ok
-      else
-        render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
-      end
+    return render json: { message: "Profile not found" }, status: :not_found unless @profile
+
+    shelf = @profile.shelves.find_by(id: params[:id])
+    return render json: { message: "Shelf not found" }, status: :not_found unless shelf
+
+    if shelf.update(shelf_params)
+      render json: ShelfSerializer.new(shelf, include: [:shelf_books, :owned_books]).serializable_hash, status: :ok
     else
-      render json: { messages: "Profile not found" }, status: :not_found
+      render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    if @profile
-      shelf = @profile.shelves.find_by(id: params[:id])
-      if shelf.destroy
-        render json: { message: "Shelf was successfully deleted." }, status: :ok
-      else
-        render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
-      end
+    return render json: { message: "Profile not found" }, status: :not_found unless @profile
+
+    shelf = @profile.shelves.find_by(id: params[:id])
+    return render json: { message: "Shelf not found" }, status: :not_found unless shelf
+
+    if shelf.destroy
+      render json: { message: "Shelf was successfully deleted." }, status: :ok
     else
-      render json: { messages: "Profile not found" }, status: :not_found
+      render json: { errors: shelf.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
-  def get_profile
+  def set_profile
     @profile = Profile.find_by(id: params[:profile_id])
   end
 
