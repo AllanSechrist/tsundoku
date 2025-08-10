342aec7262bd0e8cdd0142a63d09dc1c5b86a8d0
Allan Sechrist <sechristallan@gmail.com>
2025-08-11 07:18:13 +0900

    Add methods to get a list of user for bookclubs.

--- app/models/bookclub.rb
+++ app/models/bookclub.rb
@@ -5,9 +5,16 @@ class Bookclub < ApplicationRecord
 
   after_create :add_creator_as_member
 
+  def get_members
+    # returns a list of users in a bookclub
+    members.map { |member| member.user.email }
+  end
+
   private
 
   def add_creator_as_member
     members.create(user: creator)
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   has_many :shelves, through: :profile
-  has_many :members, dependent: :destroy
+  has_many :memberships, class_name: "Member", dependent: :destroy
   has_many :bookclubs, foreign_key: :creator_id, dependent: :destroy # maybe change to nullify in future?
   has_many :owned_books, dependent: :destroy
   has_many :books, through: :owned_books
 
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def get_memberships
+    # returns a list of bookclub names that the user is in.
+    memberships.map { |membership| membership.bookclub.name }
+  end
 end
